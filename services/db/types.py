54ce593a44d7ef092099b1deaeef6f91f306d0f6
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-04 00:46:31 +0300

    Add AnySessionAbstractContextManager annotation

--- services/db/types.py
+++ services/db/types.py
@@ -1,9 +1,10 @@
 """
 Custom typings for DB package.
 """
-from typing import TypeVar
+from typing import TypeVar, Union, ContextManager, AsyncContextManager
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
 SessionType = TypeVar('SessionType', AsyncSession, Session)
+AnySessionAbstractContextManager = Union[ContextManager[SessionType], AsyncContextManager[SessionType]]
