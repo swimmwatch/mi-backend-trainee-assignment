96c8be1e4ddb6529138102023b1edeae8b72f531
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-04 01:26:25 +0300

    Add Announcement observer service entrypoint

---
+++ services/announcement_observer/main.py
@@ -0,0 +1,41 @@
+"""
+Announcement observer service entrypoint.
+"""
+from typing import List
+
+from dependency_injector.wiring import Provide
+from fastapi import FastAPI, Depends
+
+from services.announcement_observer.container import AnnouncementObserverContainer
+from services.announcement_observer.dal import CityRepository
+from services.announcement_observer.schemas.city import City
+from services.announcement_observer.utils import async_init_db
+
+app = FastAPI()
+container = AnnouncementObserverContainer()
+
+
+@app.on_event('startup')
+async def wire_container():
+    container.wire(modules=[__name__])
+
+
+@app.on_event('startup')
+async def init_db():
+    await async_init_db(container.db())
+
+
+@app.get('/cities', response_model=List[City])
+async def get_cities(
+    cities_rep: CityRepository = Depends(
+        Provide[AnnouncementObserverContainer.cities_repository]
+    )
+):
+    cities_records = await cities_rep.get_all()
+    return [
+        City(
+            id=city_record.id,
+            name=city_record.name
+        )
+        for city_record in cities_records
+    ]
