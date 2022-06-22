3774b13150f651b81b48030bc4ffeaf754f68c8f
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-22 22:30:02 +0300

    Add Playwright request utils module

---
+++ utils/playwright/request.py
@@ -0,0 +1,22 @@
+"""
+Playwright request utils.
+"""
+from typing import Type, TypeVar
+
+from playwright.async_api import Page
+from pydantic import BaseModel, BaseSettings
+
+T = TypeVar('T', BaseModel, BaseSettings)
+
+
+async def make_async_get_request(page: Page, url: str, schema_type: Type[T]) -> T:
+    """
+    Make GET request by Playwright.
+    :param schema_type: Pydantic schema
+    :param page: Playwright page
+    :param url: URL
+    :return: Schema instance
+    """
+    response = await page.goto(url, wait_until='load')
+    content = await response.body()
+    return schema_type.parse_raw(content)
