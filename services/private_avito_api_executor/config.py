be5a8247e2ee84840b10ba3505cc92e11e484495
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-22 23:20:31 +0300

    Fix Avito magic key name

--- services/private_avito_api_executor/config.py
+++ services/private_avito_api_executor/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 
 class PrivateAvitoApiExecutorSettings(BaseSettings):
     grpc_server_addr: str
-    avito_magick_key: str
+    avito_magic_key: str
 
 
 private_avito_api_executor_settings = PrivateAvitoApiExecutorSettings()
