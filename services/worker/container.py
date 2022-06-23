6dc95a875fbcf43b0646709346f7ffa58937639d
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-24 00:44:59 +0300

    Add private_avito_api_executor_client to WorkerContainer

--- services/worker/container.py
+++ services/worker/container.py
@@ -5,7 +5,9 @@ from dependency_injector.containers import DeclarativeContainer
 from dependency_injector.wiring import providers
 from redis.client import Redis
 
+from services.private_avito_api_executor.grpc_client import PrivateAvitoApiExecutorGrpcClient
 from services.redis.config import REDIS_HOST, REDIS_PORT
+from services.worker.config import worker_settings
 
 
 class WorkerContainer(DeclarativeContainer):
@@ -13,4 +15,9 @@ class WorkerContainer(DeclarativeContainer):
         Redis,
         host=REDIS_HOST,
         port=REDIS_PORT
+    )
+
+    private_avito_api_executor_client = providers.Singleton(
+        PrivateAvitoApiExecutorGrpcClient,
+        worker_settings.private_avito_api_executor_grpc_server_addr
     )
