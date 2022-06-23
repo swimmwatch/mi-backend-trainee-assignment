64def67a2a225a6222e5a5f1bd2d524ea03b7ff5
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-23 15:27:17 +0300

    Update dummy model

--- services/amount_ads_observer/models.py
+++ services/amount_ads_observer/models.py
@@ -6,8 +6,9 @@ from sqlalchemy import Column, Integer, String
 from services.db import Base
 
 
-class City(Base):
-    __tablename__ = 'cities'
+class AdObserver(Base):
+    __tablename__ = 'ad_observers'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    location_id = Column(Integer, nullable=False)
+    query = Column(String, nullable=False)
