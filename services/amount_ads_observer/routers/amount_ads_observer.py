689f9871633c53ed816bd83fbd994a4dc6318d30
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-24 00:50:04 +0300

    Implement dummy /stat endpoint

--- services/amount_ads_observer/routers/amount_ads_observer.py
+++ services/amount_ads_observer/routers/amount_ads_observer.py
@@ -1,16 +1,25 @@
+from http import HTTPStatus
+
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
+from starlette.responses import Response
 
 from services.amount_ads_observer.container import AmountAdsObserverContainer
 from services.amount_ads_observer.dal import AdsObserversRepository
 from services.amount_ads_observer.schemas.ads_observer import AdsObserverCreate, AdsObserver
+from services.worker.app import save_curr_ads_amount
+
 
 router = APIRouter(prefix='/ads/amount/observer')
 
 
 @router.get('/stat')
-async def get_stat():
-    pass
+async def get_stat(location_id: int = Query(), query: str = Query()):
+    save_curr_ads_amount.delay(
+        location_id=location_id,
+        query=query
+    )
+    return Response(status_code=HTTPStatus.NO_CONTENT)
 
 
 @router.post('/add', response_model=AdsObserver)
