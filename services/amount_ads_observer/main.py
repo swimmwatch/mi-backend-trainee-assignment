6a50c23af207d9ed4a353f893614dd3bf48a7167
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-23 10:57:12 +0300

    Remove unnecessary code

--- services/amount_ads_observer/main.py
+++ services/amount_ads_observer/main.py
@@ -1,19 +1,16 @@
 """
 Announcement observer service entrypoint.
 """
-from typing import List
-
-from dependency_injector.wiring import Provide, inject
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 
 from services.amount_ads_observer.container import \
     AmountAdsObserverContainer
-from services.amount_ads_observer.dal import CityRepository
-from services.amount_ads_observer.schemas.city import City
+from services.amount_ads_observer.routers import api_router
 from services.amount_ads_observer.utils import async_init_db
-from services.worker.app import count
 
 app = FastAPI()
+app.include_router(api_router)
+
 container = AmountAdsObserverContainer()
 
 
@@ -25,21 +22,3 @@ async def wire_container():
 @app.on_event('startup')
 async def init_db():
     await async_init_db(container.db())
-
-
-@app.get('/cities', response_model=List[City])
-@inject
-async def get_cities(
-    cities_rep: CityRepository = Depends(
-        Provide[AmountAdsObserverContainer.cities_repository]
-    )
-) -> List[City]:
-    cities_records = await cities_rep.get_all()
-    count.delay()
-    return [
-        City(
-            id=city_record.id,
-            name=city_record.name
-        )
-        for city_record in cities_records
-    ]
