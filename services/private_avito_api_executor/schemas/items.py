22ba3f2906ce2596780a32057feb89e1003c8323
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-23 10:23:59 +0300

    Add camel case aliases for AvitoItemsInfo scheme

--- services/private_avito_api_executor/schemas/items.py
+++ services/private_avito_api_executor/schemas/items.py
@@ -1,15 +1,15 @@
 """
 Avito items schemas
 """
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from services.private_avito_api_executor.schemas.base import AvitoBaseResponse
 
 
 class AvitoItemsInfo(BaseModel):
     count: int
-    total_count: int
-    main_count: int
+    total_count: int = Field(alias='totalCount')
+    main_count: int = Field(alias='mainCount')
 
 
 class AvitoItemsResponse(AvitoBaseResponse):
