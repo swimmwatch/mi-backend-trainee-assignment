32d6e75c52cb5882688f89eec03974f74199a38d
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-24 00:45:21 +0300

    Add worker settings

--- services/worker/config.py
+++ services/worker/config.py
@@ -3,7 +3,16 @@ Worker configuration.
 """
 import os
 
+from pydantic import BaseSettings
+
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL',
                                    'redis://localhost:6379')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND',
                                        'redis://localhost:6379')
+
+
+class WorkerSettings(BaseSettings):
+    private_avito_api_executor_grpc_server_addr: str = 'localhost:50051'
+
+
+worker_settings = WorkerSettings()
