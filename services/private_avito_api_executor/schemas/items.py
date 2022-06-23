6ef52a5fb285d48f0880d5b8112bde6d7daa7bcb
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-23 23:05:07 +0300

    Add AvitoItemsRequest scheme

--- services/private_avito_api_executor/schemas/items.py
+++ services/private_avito_api_executor/schemas/items.py
@@ -1,6 +1,8 @@
 """
 Avito items schemas
 """
+from enum import Enum
+
 from pydantic import BaseModel, Field
 
 from services.private_avito_api_executor.schemas.base import BaseAvitoResponse
@@ -14,3 +16,15 @@ class AvitoItemsInfo(BaseModel):
 
 class AvitoItemsResponse(BaseAvitoResponse):
     result: AvitoItemsInfo
+
+
+class AvitoItemsCountState(Enum):
+    TRUE = 1
+    FALSE = 0
+
+
+class AvitoItemsRequest(BaseModel):
+    key: str
+    location_id: int = Field(alias='locationId')
+    query: str
+    count_only: AvitoItemsCountState = AvitoItemsCountState.TRUE
