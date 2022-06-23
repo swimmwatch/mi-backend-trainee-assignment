f767d7136b95096ac010f5336e34f85167986afa
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-23 23:35:13 +0300

    Add default values for settings field

--- services/private_avito_api_executor/config.py
+++ services/private_avito_api_executor/config.py
@@ -5,8 +5,8 @@ from pydantic import BaseSettings
 
 
 class PrivateAvitoApiExecutorSettings(BaseSettings):
-    grpc_server_addr: str
-    avito_magic_key: str
+    grpc_server_addr: str = 'localhost:50051'
+    avito_magic_key: str = ''
 
 
 private_avito_api_executor_settings = PrivateAvitoApiExecutorSettings()
