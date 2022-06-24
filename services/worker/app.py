5a12a92399e2411506aeb4d9f76be05dcebec348
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-24 13:59:27 +0300

    Implement save_curr_ads_amount task

--- services/worker/app.py
+++ services/worker/app.py
@@ -1,11 +1,15 @@
+from datetime import datetime, timedelta
+
 from celery import Celery
 from dependency_injector.wiring import inject, Provide
-from loguru import logger
 
+from services.amount_ads_observer.models import AdsObserver, AdsObserverStat
+from services.db import Database
 from services.private_avito_api_executor import GetAmountAdsRequest
 from services.private_avito_api_executor.grpc_client import PrivateAvitoApiExecutorGrpcClient
 from services.worker.config import worker_settings
 from services.worker.container import WorkerContainer
+from utils.datetime import get_uts_from_datatime
 
 celery = Celery(
     'tasks',
@@ -23,14 +27,43 @@ def init_di_container(sender, **kwargs):
 @celery.task
 @inject
 def save_curr_ads_amount(
-    location_id: int,
-    query: str,
-    private_avito_api_executor_client: PrivateAvitoApiExecutorGrpcClient =
-    Provide[WorkerContainer.private_avito_api_executor_client]
+        ads_observer_id: int,
+        location_id: int,
+        query: str,
+        db: Database = Provide[WorkerContainer.db],
+        private_avito_api_executor_client: PrivateAvitoApiExecutorGrpcClient =
+        Provide[WorkerContainer.private_avito_api_executor_client]
 ):
+    with db.session() as session:
+        exists = session.query(AdsObserver) \
+                    .filter_by(id=ads_observer_id) \
+                    .exists()
+    if not exists:
+        return
+
     req = GetAmountAdsRequest(
         location_id=location_id,
         query=query
     )
     res = private_avito_api_executor_client.stub.get_amount_ads(req)
-    logger.info(res.amount)
+
+    now = datetime.utcnow()
+    uts_now = get_uts_from_datatime(now)
+    with db.session() as session:
+        ads_observer_stat = AdsObserverStat(
+            ads_observer_id=ads_observer_id,
+            timestamp=uts_now,
+            amount=res.amount
+        )
+        session.add(ads_observer_stat)
+        session.commit()
+
+    in_hour = now + timedelta(minutes=1)
+    save_curr_ads_amount.apply_async(
+        (
+            ads_observer_id,
+            location_id,
+            query
+        ),
+        eta=in_hour
+    )
