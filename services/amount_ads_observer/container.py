3a00b81de5ae78577ac17d6468cf9b7b2493e1cd
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-24 13:29:39 +0300

    Create DatabaseSettings class. Replace constants to settings instance

--- services/amount_ads_observer/container.py
+++ services/amount_ads_observer/container.py
@@ -6,13 +6,13 @@ from dependency_injector.wiring import providers
 
 from services.amount_ads_observer.dal import AdsObserversRepository
 from services.db import AsyncDatabase
-from services.db.config import DB_URL
+from services.db.config import database_settings
 
 
 class AmountAdsObserverContainer(DeclarativeContainer):
     db = providers.Singleton(
         AsyncDatabase,
-        db_url=DB_URL
+        db_url=database_settings.db_url
     )
     ads_observers_repository = providers.Factory(
         AdsObserversRepository,
--- services/db/config.py
+++ services/db/config.py
@@ -1,11 +1,19 @@
 """
 Database configuration.
 """
-from os import environ
+from pydantic import BaseSettings
 
-DB_USER = environ.get('DB_USER', 'postgres')
-DB_PASSWORD = environ.get('DB_PASSWORD')
-DB_NAME = environ.get('DB_NAME')
-DB_HOST = environ.get('DB_HOST', 'localhost')
-DB_SCHEME = 'postgresql+asyncpg'
-DB_URL = f'{DB_SCHEME}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
+class DatabaseSettings(BaseSettings):
+    db_user: str = 'postgres'
+    db_password: str = ''
+    db_name: str = ''
+    db_host: str = 'localhost'
+    db_scheme: str = 'postgresql+asyncpg'
+
+    @property
+    def db_url(self):
+        return f'{self.db_scheme}://{self.db_user}:{self.db_password}@{self.db_host}/{self.db_name}'
+
+
+database_settings = DatabaseSettings()
