552d9a49da78db584b73ab6806c2e8c0281380f0
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-06-26 15:04:45 +0300

    Move grpc_client.py to grpc/client.py

--- services/worker/app.py
+++ services/worker/app.py
@@ -7,7 +7,7 @@ from dependency_injector.wiring import inject, Provide
 from services.amount_ads_observer.dal import AdsObserversRepository, AdsObserversStatRepository
 from services.amount_ads_observer.schemas.ads_observer_stat import AdsObserverStatCreate
 from services.private_avito_api_executor import GetAmountAdsRequest
-from services.private_avito_api_executor.grpc_client import PrivateAvitoApiExecutorGrpcClient
+from services.private_avito_api_executor.grpc.client import PrivateAvitoApiExecutorGrpcClient
 from services.worker.config import worker_settings
 from services.worker.container import WorkerContainer
 from utils.datetime import get_uts_from_datatime
