fe83b2280d3101a06dd86795b2cd25c4ae4d2c80
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2022-07-25 17:37:59 +0300

    Fix get_uts_from_datetime function.

--- utils/datetime.py
+++ utils/datetime.py
@@ -9,6 +9,5 @@ def get_uts_from_datetime(dt: datetime) -> int:
     :param dt: datatime instance
     :return: Unix timestamp
     """
-    current_datetime = dt.utcnow()
-    current_timetuple = current_datetime.utctimetuple()
+    current_timetuple = dt.utctimetuple()
     return calendar.timegm(current_timetuple)
